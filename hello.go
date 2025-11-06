b175a811e6d1920d4576d7b2a877e8eb94c08042
Felipe Nogueira <felnog25@gmail.com>
2025-11-06 12:00:50 -0300

    Atualiza exercicio incluindo variaveis hello.go

--- hello.go
+++ hello.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Olá, mundo. Bem Vindo!")
+	var nome string = "Felipe"
+	var idade int
+	var versao float32 = 1.1
+	fmt.Println("Olá, Sr.", nome)
+	fmt.Println("Olá Sr.", nome, "sua idade é:", idade)
+	fmt.Println("Este programa está na versão", versao)
 }
