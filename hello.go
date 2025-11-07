85d4855c3cc18c6f2cb43234e43ec6282e78337b
Felipe Nogueira <felnog25@gmail.com>
2025-11-07 17:53:48 -0300

    Atualiza hello.go com criacao da estrutura de monitoramento de sites

--- hello.go
+++ hello.go
@@ -1,12 +1,69 @@
+// hello.go
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-	var nome string = "Felipe"
-	var idade int
-	var versao float32 = 1.1
-	fmt.Println("Olá, Sr.", nome)
-	fmt.Println("Olá Sr.", nome, "sua idade é:", idade)
+
+	exibeIntroducao()
+
+	for {
+
+		exibeMenu()
+		comando := leComando()
+		iniciarMonitoramento()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
+
+	}
+
+}
+
+func exibeIntroducao() {
+	nome := "Felipe"
+	versao := 1.1
+	fmt.Println("Olá, sr(a).", nome)
 	fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O valor da variável comando é:", comandoLido)
+
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	// site com URL inexistente
+	site := "https://httpbin.org/status/404" // ou 200
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
