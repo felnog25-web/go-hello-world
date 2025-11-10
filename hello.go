6af5fd21efed12058c0f460ae217012a1a0f78d6
Felipe Nogueira <felnog25@gmail.com>
2025-11-10 17:02:23 -0300

    Execicio agora com Slice, Array e For

--- hello.go
+++ hello.go
@@ -1,21 +1,21 @@
-// hello.go
 package main
 
 import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
+const monitoramentos = 3
+const delay = 5
+
 func main() {
-
 	exibeIntroducao()
-
 	for {
-
 		exibeMenu()
+
 		comando := leComando()
-		iniciarMonitoramento()
 
 		switch comando {
 		case 1:
@@ -23,21 +23,20 @@ func main() {
 		case 2:
 			fmt.Println("Exibindo Logs...")
 		case 0:
-			fmt.Println("Saindo do programa...")
+			fmt.Println("Saindo do programa")
 			os.Exit(0)
 		default:
 			fmt.Println("Não conheço este comando")
 			os.Exit(-1)
 		}
-
 	}
 
 }
 
 func exibeIntroducao() {
 	nome := "Felipe"
-	versao := 1.1
-	fmt.Println("Olá, sr(a).", nome)
+	versao := 1.2
+	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
 }
 
@@ -50,20 +49,36 @@ func exibeMenu() {
 func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
-	fmt.Println("O valor da variável comando é:", comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
 
 	return comandoLido
 }
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	// site com URL inexistente
-	site := "https://httpbin.org/status/404" // ou 200
+
+	sites := []string{"https://httpbin.org/status/200",
+		"https://www.alura.com.br", "https://www.caelum.com.br"}
+
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+	fmt.Println("")
+}
+
+func testaSite(site string) {
 	resp, _ := http.Get(site)
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
 	} else {
-		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
 	}
 }
